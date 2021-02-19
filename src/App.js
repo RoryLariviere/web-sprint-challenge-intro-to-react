19ace3a130f5c75f19230f4e5ad2d198cc63b0b4
Rory Lariviere <rorydlar@gmail.com>
2021-02-19 12:24:27 -0600

    mvp potentially met, implemented styled components and made name/date elements

--- src/App.js
+++ src/App.js
@@ -1,18 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import Details from './components/Details';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const StyledH1 = styled.h1`
+  font-weight: bolder;
+  font-size: 2.5rem;
+  color: #FFE81F;
+  text-shadow: 2px 2px black;
+  background-color: #444444;
+  border-top: 3px solid red;
+  border-bottom: 3px solid red;
+  border-left: 3px solid #FFE81F;
+  border-right: 3px solid #FFE81F;
+  padding: 20px;
+  border-radius: 25px 0px 25px 0px;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characterData, setCharacterData] = useState([]);
+  const [currentCharUrl, setCurrentCharUrl] = useState(null);
+  console.log(characterData);
+
+  const openDetails = url => {
+    setCurrentCharUrl(url);
+  }
+
+  const closeDetails = () => {
+    setCurrentCharUrl(null);
+  }
+
+  useEffect(()=>{
+    axios.get('https://swapi.dev/api/people/')
+      .then(res=>{
+        setCharacterData(res.data);
+      })
+      .catch(err=>{
+        console.log(err);
+      });
+  }, []);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <StyledApp className="App">
+      <StyledH1 className="Header">Characters</StyledH1>
+      {
+        characterData.map(char=>{
+          return <Character key={char.url} info={char} action={openDetails} />
+        })
+      }
+      {
+        currentCharUrl && <Details charUrl={currentCharUrl} close={closeDetails} />
+      }
+    </StyledApp>
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,37 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    background-color: #444444;
+    border: 3px solid crimson;
+    border-radius: 25px;
+    font-weight: bolder;
+    font-size: 1.5rem;
+    color: #FFE81F;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    width: 30vw;
+    margin-top: 10px;
+    cursor: pointer;
+`
+const StyledName = styled.p`
+    padding-left: 10px;
+    padding-right: 10px;
+`
+const StyledDate = styled.p`
+    
+    padding-left: 10px;
+    padding-right: 10px;
+`
+
+export default function Character({info, action}) {
+    return (
+        <StyledDiv onClick={() => action(info.url)}>
+            <StyledName>{info.name}</StyledName>
+            <StyledDate>{info.birth_year}</StyledDate>
+        </StyledDiv>
+    )
+}
